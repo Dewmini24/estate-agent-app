3d075e7173fc96bddd5778039e4dcd94e74e81e6
Dewmini24 <dewminipathirana04@gmail.com>
2026-07-09 09:43:33 +0530

    feat: wire up routing and FavouritesProvider in App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "react-widgets/styles.css";
+import "react-tabs/style/react-tabs.css";
+import "./index.css";
+import App from "./App.jsx";
+import { FavouritesProvider } from "./context/FavouritesContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <FavouritesProvider>
+        <App />
+      </FavouritesProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
